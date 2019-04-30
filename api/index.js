0c8748ce1aa7876c5df15f77ca09626640311a24
guillem.casanova <guillemcasanova@gmail.com>
2019-05-01 01:01:41 +0200

    first commit api running

---
+++ api/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var search = require('./search/search.js')
+var app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+app.get('/search', search.getFirst)
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
---
+++ api/search/search.js
@@ -0,0 +1,24 @@
+var request = require('request')
+var cheerio = require('cheerio')
+var url     = require('url')
+
+
+exports.getFirst = function(req, res) {
+  const text = req.query.q
+  request('http://www.google.com/search?q='+ text, function (error, response, body) {
+    if (error){
+      console.log('error:', error); // Print the error if one occurred
+    } else {
+      var $ = cheerio.load(body)
+      const result = $('#search .g').first()
+      var item = {};
+      var elemUrl = result.find("h3 a")
+      var parsedUrl = url.parse(elemUrl.attr("href"), true)
+      if (parsedUrl.pathname === '/url') {
+        item['url'] = parsedUrl.query.q
+      }
+      res.send(item['url'])  
+    }  
+  })
+
+}
