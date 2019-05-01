3e5d76dddcea9abe014c86638f856500d185e705
guillem.casanova <guillemcasanova@gmail.com>
2019-05-01 11:53:37 +0200

    Add some coments in tho code

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var search = require('./search/search.js');
 var app = express();
 
+// Allow CORS
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
@@ -10,10 +11,12 @@ app.use((req, res, next) => {
   next();
 });
 
+// If get / redirect to the land-page to do the search
 app.get('/', function (req, res) {
   res.redirect('https://keen-noether-d8b69f.netlify.com');
 });
 
+// Do the search in google of the string passed as a parameter 
 app.get('/search', search.getFirst);
 
 const port = process.env.PORT || 3000;
--- api/search/search.js
+++ api/search/search.js
@@ -1,8 +1,9 @@
 var request = require('request');
 var cheerio = require('cherio');
-var url     = require('url');
+var url = require('url');
 
-
+// The request contains a string with the words to find in google website
+// The function returns the first url that appears in google
 exports.getFirst = function(req, res) {
   const text = req.query.q;
   const url2search = 'http://www.google.com/search?q='+ text.toString();
@@ -10,6 +11,7 @@ exports.getFirst = function(req, res) {
   request(url2search, function (error, response, body) {
     if (error){
       console.log('error:', error); // Print the error if one occurred
+      res.status(500).send('Something broke!'); // Error doing the request to google
     } else {
       var $ = cheerio.load(body);
       const result = $('#search .g').first();
@@ -17,7 +19,7 @@ exports.getFirst = function(req, res) {
       var elemUrl = result.find("h3 a");
       console.log("the elem URL recibed is:", elemUrl.attr("href"));
       if (elemUrl.attr("href") === undefined){
-        res.status(500).send('Something broke!');
+        res.status(500).send('Something broke!'); //The url recibed is not an URL
       } else {
         var parsedUrl = url.parse(elemUrl.attr("href"), true);
         if (parsedUrl.pathname === '/url') {
