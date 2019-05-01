b670ee6d0bdd2ebc3fd24900380fba77bcce9dfd
guillem.casanova <guillemcasanova@gmail.com>
2019-05-01 09:52:57 +0200

    add the port variable in the index file

--- api/index.js
+++ api/index.js
@@ -12,7 +12,8 @@ app.use((req, res, next) => {
 
 app.get('/search', search.getFirst)
 
+const port = process.env.PORT || 8000;
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+  console.log('Example app listening on port' + port);
 });
