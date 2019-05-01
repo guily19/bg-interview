01529d4f5b994a192462ffcf6ac47b065179a38e
guillem.casanova <guillemcasanova@gmail.com>
2019-05-01 10:09:54 +0200

    Change the URL from localhost to heroku web app

--- landing-page/js/button-function.js
+++ landing-page/js/button-function.js
@@ -5,7 +5,7 @@
       alert("There is no text in the box");      
     } else {
       var req = new XMLHttpRequest();
-      req.open('GET', 'http://localhost:3000/search?q=' + text, true);
+      req.open('GET', 'https://boiling-sands-23849.herokuapp.com/search?q=' + text, true);
       req.onreadystatechange = function (aEvt) {
         if (req.readyState == 4) {
           if(req.status == 200)
