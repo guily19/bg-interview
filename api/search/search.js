35cf37f9805bc552c528b8e832d3aebf87f70d7d
guillem.casanova <guillemcasanova@gmail.com>
2019-05-01 09:41:25 +0200

    Add console log to check the text variable

--- api/search/search.js
+++ api/search/search.js
@@ -5,6 +5,7 @@ var url     = require('url')
 
 exports.getFirst = function(req, res) {
   const text = req.query.q
+  console.log('the text to search is:', text)
   request('http://www.google.com/search?q='+ text, function (error, response, body) {
     if (error){
       console.log('error:', error); // Print the error if one occurred
