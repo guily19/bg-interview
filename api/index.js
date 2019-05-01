74141272db83a7834f828556417f3fa454b3af22
guillem.casanova <guillemcasanova@gmail.com>
2019-05-01 10:04:43 +0200

    change the port where is listening to 3000

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ app.get('/', function (req, res) {
 
 app.get('/search', search.getFirst)
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, function () {
   console.log('Example app listening on port' + port);
